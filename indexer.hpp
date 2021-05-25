e560cfaf657f3397ac8cc8a657535f4c98a42658
Gohar Simonyan <82412056+goharsimonyann@users.noreply.github.com>
2021-05-26 01:07:05 +0400

    Update indexer.hpp

--- indexer.hpp
+++ indexer.hpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <pthread.h>
 using namespace std;
 
 class Indexer
@@ -12,5 +13,5 @@ public:
 private:
     //word -> (relevance, URL)
     map<string, map<float, string>> _index;
-
+    pthread_mutex_t _mutex = PTHREAD_MUTEX_INITIALIZER;
 };
